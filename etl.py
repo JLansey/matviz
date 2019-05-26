c7d385c0dc93645e2b37bdefc76498c72625966c
Jonathan Lansey <lansey+github@gmail.com>
2019-05-26 16:32:21 -0400

    first public release

---
+++ etl.py
@@ -0,0 +1,446 @@
+
+import os,sys,csv
+
+
+# regular pythong stuff
+# redundant form interactive computing
+import collections
+from collections import defaultdict
+from collections import Sequence
+from itertools import chain, count
+import glob
+import json
+
+# datetime related things
+from dateutil.parser import parse as dateutil_parse
+import matplotlib.dates as md # for handledates
+import datetime
+from pytz import timezone
+
+
+# regular anaconda stuff
+import numpy as np
+from scipy import signal
+import pandas as pd
+
+
+# useful stuffs:
+from numpy import diff
+
+
+data_dir = 'data'
+fig_dir = 'figs'
+
+eps = np.spacing(1)
+
+
+
+def time_delta_to_days(w):
+    return w / np.timedelta64(1, 'D')
+
+
+
+def get_object_size(obj):
+    the_size = sys.getsizeof(obj)*1e-6
+    print("Object size in MB: {0:.2f}".format(the_size))
+    # return the_size
+
+
+def pprint_entire_df(df):
+    with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+        print(df)
+
+
+
+def sql(query, db):
+    """
+    Parameters:
+        query: A sql query written in normal sql language inside quotes,
+            type = string
+        db: from_db.sql_connection()
+
+    Returns:
+        a dictof sql data
+    """
+    db.execute(query)
+    results = db.fetchall()
+
+    return results
+
+def sql_redshift(query,db,array):
+    db.execute(query,array)
+    results = db.fetchall()
+    db.close()
+    return results
+
+
+def subsetter(results,vars):
+    # in case you only want a single variable returned
+    # is there a check for just a scalar??
+    # Note that this subsetter only works for resultsets
+
+    if type(vars) not in [list,set]:
+        vars = [vars]
+
+    if len(results)>0:
+        return [{k:v for k,v in dict(cur_row).items() if k in vars} for cur_row in results]
+    else:
+        return []
+
+
+
+def remove_tz(cur_datetime):
+    if hasattr(cur_datetime,"__len__"):
+        naive_datetime = [w.replace(tzinfo=None) for w in cur_datetime]
+    else:
+        naive_datetime = cur_datetime.replace(tzinfo=None)
+    return naive_datetime
+
+def tz_to_utc(cur_datetime,local_tz='US/Eastern',native=True):
+    local_datetime = timezone(local_tz).localize(cur_datetime)
+    utc_datetime = local_datetime.astimezone(timezone('UTC'))
+
+    if native:
+        return utc_datetime.replace(tzinfo=None)
+    else:
+        return utc_datetime
+
+def utc_to_tz(cur_utc,local_tz='US/Eastern'):
+    # this is a UTC time but without timezone information
+    utc_datetime = timezone('UTC').localize(cur_utc)
+    local_datetime = utc_datetime.astimezone(timezone(local_tz))
+    naive_datetime = local_datetime.replace(tzinfo=None)
+    return naive_datetime
+
+def to_tz(cur_tz,local_tz='US/Eastern'):
+    # if there is already time zone information associated here, then just go with it
+    local_datetime = cur_tz.astimezone(timezone(local_tz))
+    naive_datetime = local_datetime.replace(tzinfo=None)
+    return naive_datetime
+
+
+def average_times(time_1,time_2):
+    dummy_time = datetime.datetime(2000, 1, 1, 0, 0)
+    return dummy_time + datetime.timedelta(seconds=((time_1-dummy_time).total_seconds()+(time_2-dummy_time).total_seconds())/2)
+
+
+
+
+def chunks(l, n):
+    """Yield successive n-sized chunks from l."""
+    for i in range(0, len(l), n):
+        yield l[i:i+n]
+
+
+def parse_min_sec(time_str):
+    '''
+    convert normal times into seconds
+    gosh, surprising that there wasn't already some way to do this robustly
+    in python. Note that this does not work if you've got hours
+    '''
+    min_sec = time_str.split(":")
+    if len(min_sec)>2:
+        raise Exception("we don't do hours yet folks")
+    elif len(min_sec)>1:
+        minn = 60*float(min_sec[0])
+        secc = min_sec[1]
+    else:
+        minn = 0
+        secc = min_sec[0]
+    return minn + float(secc)
+
+
+
+def clean_whitespace(my_str):
+
+    my_str = my_str.replace('\n','')
+    my_str = my_str.replace('\r','')
+    my_str = my_str.replace('\t',' ')
+    for ii in range(10):
+        my_str = my_str.replace('  ',' ')
+
+    if my_str:
+        if my_str[0] == ' ':
+            my_str = my_str[1:]
+        if my_str[-1] == ' ':
+            my_str = my_str[:-1]
+
+    return my_str
+
+# This essentially splits a giant list by
+# some function.
+def full_group_by(l, key=lambda x: x):
+    d = defaultdict(list)
+    for item in l:
+        d[key(item)].append(item)
+    return d
+
+
+def read_csv(name,qt = 1):
+  return list(csv.reader(open(name),quoting = qt))
+
+def write_csv(name,array,param='w'):
+    with open(name, param) as f:
+        writer = csv.writer(f,quoting =1)
+        writer.writerows(array)
+    return True
+
+
+def write_csv2(name,array,param='w'): # a for append
+    with open(name, param) as f:
+        writer = csv.writer(f,quoting =1)
+        writer.writerows(array)
+    return True
+
+
+# accept a data frame with only two columns, and make first the key and second the value
+def dictify_cols(df):
+    return df.set_index(df.columns[0])[df.columns[1]].to_dict()
+
+def dictify_csv(weird_array,headers = None):
+    # turns a csv into a dictionary with long columns
+    '''
+    Args:
+        weird_array: just an array with items in the rows
+
+    Returns:
+        a dictionary where the header row becomes the keys for the dict
+    '''
+    if headers:
+        dicto = dictify_csv([headers]+weird_array)
+    else:
+        dicto =  {w[0]:w[1:] for w in np.transpose(weird_array)}
+
+    return dicto
+
+
+def csvify_dict(weird_dict):
+    '''
+    # turn a dictionary with long columns into a thing you can print
+    Args:
+        weird_dict:
+         just a dictionary
+
+    Returns:
+        a list of lists ready to be turned into a csv
+
+    '''
+    # initialize it to be on more
+    headers = []
+    csvo = [[] for w in next(iter(weird_dict.values()))] # [[]]+
+    for k,v in weird_dict.items():
+        headers.append(k)
+        for ii in range(len(v)):
+            csvo[ii].append(str(v[ii]))
+
+    return [headers]+csvo
+
+
+
+def nan_smooth(y,n=5,ens=[],ignore_nans=True):
+    """
+    Args:
+        y: your timeseries (don't need x)
+        n: int-> window is hanning length n
+           list-> window is exactly the list you pass,[1,1,1,1]/4 for moving average
+        ens: weighting of the points in 'y' in
+        ignore_nans: if you want to ignore nans, or exclude data that has nans in it
+
+    Returns:
+        Smoothed values, centered, and same dimension as input
+
+    """
+
+    # just in case you didn't pass in a numpy datatype
+    y = np.array(y)
+    ens = np.array(ens)
+
+    # handle weird cases
+    if len(y)<3:
+        raise Warning('Sorry bud, you can''t smooth less than 3 points, thats silly')
+        return y
+
+    if len(ens)==0:
+        ens = np.ones(len(y))
+        ens[np.isnan(y)]=0
+
+    elif len(ens)!=len(y):
+        raise Exception('ens must be the same length as y')
+
+    # user did not pass a window function in
+    if np.isscalar(n):
+        if n<2:
+            # windows size is 1, that is silly, no smoothing is happenening
+            return y
+        # create a nice smoothing window
+        # normal distribution with 95% confidence bounds
+        # tmp = np.linspace(-1.96,1.96,n)
+        # window = np.exp(-np.power(tmp,2))/sum(np.exp(-np.power(tmp,2)))
+        # so that n=1 represents window [0 1 0]
+        window = signal.hann(n+2)
+    else:
+        window = n
+        if round(sum(window)*100000)/100000 != 1:
+            raise Warning('the sum of your window does not equal to one. Your smoother will be biased')
+        n = len(window)
+
+    if len(y)<=n:
+        raise Warning('Sorry bud, you can''t smooth that, pick a smaller n')
+        return y
+
+    # ignore those nans
+    if ignore_nans:
+        ens[np.isnan(y)]=0
+        y[ens==0]=0
+    else:
+        y[ens==0]=np.nan
+
+    # yup, these three lines do all the real work ... VERY FAST with FFT
+    ys = signal.convolve(y*ens, window, mode='same')
+    enss = signal.convolve(ens, window, mode='same')
+    # normalize things.
+    outt = ys/enss
+
+
+    #  defining a subfunction in case you want to test it out
+    def test_nan_smooth():
+
+        N2=50
+        x2 = np.linspace(0,8*np.pi,N2)
+        ens2 = np.random.randint(0,30,N2)+20
+        noise2 = [np.mean(np.random.normal(0,1,w)) for w in ens2]
+        sig2 = np.sin(x2)
+        y2 = sig2 + np.array(noise2)
+
+        #####
+        win_width = 18
+        # plot.clf()
+        # plot(x2,y2,'o-',color=".65",lw=4)
+        # plot(x2,sig2,'r',lw=3)
+        # plot(x2,nan_smooth(y2,[],np.ones(win_width)/win_width),'g',lw=3)
+        # plot(x2,nan_smooth(y2,ens2,win_width),'b',lw=3)
+        # grid(False)
+        # legend(['noisy signal','true signal','moving average','nan_smoothed'])
+        # nicefy()
+
+    return outt
+
+
+def reverse_dict(tmp_dict):
+    return {v: k for k, v in tmp_dict.items()}
+
+
+def recurse_func(my_list,my_func,stop_level=False):
+    """
+    Will compute some function, at some level down.
+    Args:
+        my_list:
+        my_func:
+        stop_level:
+
+    Returns:
+
+    """
+    if list_depth(my_list) == stop_level:
+        return my_func(my_list)
+    elif hasattr(my_list,"__len__"):
+        return [recurse_func(w,my_func,stop_level) for w in my_list]
+    else:
+        return my_func(my_list)
+# http://stackoverflow.com/questions/6039103/counting-deepness-or-the-deepest-level-a-nested-list-goes-to
+
+def list_depth(seq):
+    seq = iter(seq)
+    try:
+        for level in count():
+            seq = chain([next(seq)], seq)
+            seq = chain.from_iterable(s for s in seq if isinstance(s, Sequence))
+    except StopIteration:
+        return level
+
+def flatten_list(my_list):
+    return list(flatten(my_list))
+
+def flatten(my_list):
+    for el in my_list:
+        if isinstance(el, collections.Iterable) and not isinstance(el, (str, bytes)):
+            for sub in flatten(el):
+                yield sub
+        else:
+            yield el
+# http://stackoverflow.com/questions/2158395/flatten-an-irregular-list-of-lists-in-python/2158532#2158532
+
+
+# convert dates to numbers, probably needs improvement or a complete overhaul, maybe moved to etl
+
+def handle_dates(X):
+    # in case you passed a date object better convert that
+    if isinstance(X[0][0],datetime.time):
+        start_day = datetime.date(2000, 1, 1)
+        # HOURS_PER_DAY = 24
+        # MINUTES_PER_DAY = 24*60
+        # X = [[(w.hour / HOURS_PER_DAY + w.minute / MINUTES_PER_DAY) for w in x] for x in X]
+        X = [[md.date2num(datetime.datetime.combine(start_day,w)) for w in x] for x in X]
+        datetime_flag = True
+        date_formatting = '%H:%M:%S'
+    else:
+        if isinstance(X[0][0],datetime.datetime) or isinstance(X[0][0],datetime.date):
+            X = [md.date2num(x) for x in X]
+            datetime_flag = True
+
+            global_min = min(np.concatenate(X))
+            global_max = max(np.concatenate(X))
+            global_range = global_max - global_min
+#           if the range is greater than 3 days
+            if global_range > 3:
+                date_formatting = '%Y-%m-%d'
+            else:
+                date_formatting = '%Y-%m-%d %H:%M:%S'
+
+        else:
+            datetime_flag = False
+            date_formatting = 'N/A'
+    return X, datetime_flag, date_formatting
+
+
+
+def start_and_ends(logical_array):
+    """
+     Return the starts and end times for when the logical
+     array True
+    :param logical_array:
+    :return:
+    list of (start,end) tuples of the indexes
+
+    Note: if the array starts with a [True, False], you completely
+          miss it because it technically *ended at that point
+          and started before the logical array began
+          If the array starts with [True,True, False]
+          then you get [(0,1),...]
+    #
+
+    """
+
+    # Padd the array with Falses to get the ends
+    padded_array = np.concatenate(([False],logical_array,[False]))
+
+    #
+    idxs = np.array(range(len(padded_array)-1))
+    differences = np.diff([np.int(w) for w in padded_array])
+    starts = idxs[differences>0]
+    ends   = idxs[differences<0]
+
+    # we added an element, now we take it away
+    starts_shift = np.maximum(starts-1,0)
+    # easier than doing a check if its empty
+    ends_shift   = np.maximum(ends-1,0)
+
+    return list(zip(starts_shift,ends_shift))
+
+
+
+def form_day(key):
+    return str(key.month) + "/" + str(key.day)
+
+def form_year(key):
+    return str(key.year) + "-" + str(key.month)
---
+++ viz.py
@@ -0,0 +1,476 @@
+''' random functions that are helpful
+'''
+
+import matplotlib
+import matplotlib.pyplot as plt
+
+from matplotlib.pyplot import plot,hist,figure,clf,cla,hold,xlabel,ylabel,xlim,ylim,\
+                              gcf,gca,close,title,legend,grid,bar,suptitle,show,\
+                              xticks,yticks
+
+import numpy as np
+import datetime
+from datetime import datetime as dt
+
+from scipy import stats
+import seaborn as sns
+from etl import nan_smooth
+
+
+def plot_range(events,color='#0093e7',offset=0):
+    # Fill registered cur_event times
+    for cur_event in events:
+        plt.fill_between([cur_event[0], cur_event[1]],
+                         [1+offset,1+offset],[offset,offset],
+                         color=color, label='Event',alpha=0.5)
+
+
+def set_fontsize(f_size=15):
+    ax = plt.gca()
+    [w.set_fontsize(f_size) for w in ([ax.title, ax.xaxis.label, ax.yaxis.label] +
+                                                    ax.get_xticklabels() +
+                                                    ax.get_yticklabels())]
+    return ax
+
+
+def print_fig_fancy(fpathh,dpi=300):
+    plt.savefig(fpathh, dpi=dpi)
+
+
+def make_title(title_str,format=False):
+
+    if title_str:
+
+        lower_case_words = {'in','the','a','and','of'}
+        upper_case_words = {}
+
+        # title_str = 'a_time_in_bed and fish'
+    #     split by
+        tmp = title_str.replace("_"," ").split()
+        if format:
+            upper_words = [w[0].upper()+w[1:] if len(w)>1 else w[0].upper() for w in tmp]
+            lower_words = [w[0].lower()+w[1:] if w.lower() in lower_case_words else w for w in upper_words]
+        else :
+            lower_words = [w.lower() for w in tmp]
+            # lower_words[0][0]=lower_words[0][0].upper()
+            lower_words[0] = lower_words[0][0].upper() + lower_words[0][1:]
+
+        upper_words =  [w.upper() if w.upper() in upper_case_words else w for w in lower_words]
+
+
+        combined_words = ' '.join(upper_words)
+    else:
+        combined_words = ''
+
+    return combined_words
+
+def large_fig(fig_num=1):
+    plt.figure(fig_num,figsize=(15,8))
+
+
+def fancy_plotter(x,y,marker_style='o',line_styles=None):
+    '''
+    will plot x and y along with a fancy trend line
+
+    '''
+
+    if line_styles==None:
+        line_styles = {'color':'0.4','lw':3}
+
+    # check that these are not regular lists, so that we can use logical indexing later
+    if type(y)!=np.ndarray:
+        y=np.array(y)
+    if type(x)!=np.ndarray:
+        x = np.array(x)
+
+    plt.hold(False)
+    plt.plot(x,y,marker_style)
+    plt.hold(True)
+    # if
+    I1 = np.logical_not(np.isnan(y))
+    I2 = np.logical_not(np.isnan(x))
+    I = np.logical_and(I1,I2)
+    if sum(I)>3:
+        m, b = np.polyfit(x[I], y[I], 1)
+        plt.plot(x, m*np.array(x) + b,**line_styles)
+
+
+# plot a diagonal line with x=y to see if your predictions are biased
+def plot_diag(lw=1):
+    ax = plt.gca()
+    ex = ax.get_xlim()
+    yy = ax.get_ylim()
+    if np.diff(ex)[0]>np.diff(yy)[0]:
+        y = yy
+        x = yy
+    else:
+        y=ex
+        x=ex
+    plt.plot(x,y,'--',color='.5', lw=lw)
+
+
+def plot_zero(lw=1, lineheight=0, linecolor='.5', style='--'):
+    ax = plt.gca()
+    ex = ax.get_xlim()
+    x = ex
+    y = [lineheight, lineheight]
+    return plt.plot(x,y,style, color=linecolor, lw=lw)
+
+
+def bar_centered(y,**kwargs):
+#     its like a regular bar plot, except that it is centered on 1:N integers
+    x = np.arange(len(y))+1
+    h = plt.bar(x,y,align='center',**kwargs)
+    plt.xticks(x)
+    return h
+
+
+
+def subplotter(x,y,n):
+    # a subplotter function that works like the matlab one does but with index starting at 0
+    tupp = (x,y)
+    cnt = 0
+    for ii in range(tupp[0]):
+        for jj in range(tupp[1]):
+            if cnt==n:
+                return plt.subplot2grid(tupp, (ii, jj))
+            cnt+=1
+    raise Exception("You have only " + str(x*y) + " subplots, but you asked for the " + str(n) + "'th")
+
+
+def pop_all():
+    '''
+    bring all the figures hiding in the background to the foreground
+    useful when using ipython in the terminal
+    '''
+    all_figures=[manager.canvas.figure for manager in matplotlib.\
+        _pylab_helpers.Gcf.get_all_fig_managers()]
+    [fig.canvas.manager.show() for fig in all_figures]
+    return len(all_figures)
+
+def suplabel(axis,label,label_prop=None,
+             labelpad=5,
+             ha='center',va='center'):
+    ''' Add super ylabel or xlabel to the figure
+    Similar to matplotlib.suptitle
+    axis       - string: "x" or "y"
+    label      - string
+    label_prop - keyword dictionary for Text
+    labelpad   - padding from the axis (default: 5)
+    ha         - horizontal alignment (default: "center")
+    va         - vertical alignment (default: "center")
+    '''
+    fig = plt.gcf()
+    xmin = []
+    ymin = []
+    for ax in fig.axes:
+        xmin.append(ax.get_position().xmin)
+        ymin.append(ax.get_position().ymin)
+    xmin,ymin = min(xmin),min(ymin)
+    dpi = fig.dpi
+    if axis.lower() == "y":
+        rotation=90.
+        x = xmin-float(labelpad)/dpi
+        y = 0.5
+    elif axis.lower() == 'x':
+        rotation = 0.
+        x = 0.5
+        y = ymin - float(labelpad)/dpi
+    else:
+        raise Exception("Unexpected axis: x or y")
+    if label_prop is None:
+        label_prop = dict()
+    plt.text(x,y,label,rotation=rotation,
+               transform=fig.transFigure,
+               ha=ha,va=va,
+               **label_prop)
+
+
+# Note: this is a half-complete port of my former matlab code
+# https://www.mathworks.com/matlabcentral/fileexchange/29545-power-law-exponential-and-logarithmic-fit?s_tid=prof_contriblnk
+def logfit(x, y=None, graph_type='linear', ftir=.05, marker_style='.k', line_style='--g'):
+    print(y)
+    if y is  None:
+        y = x
+        x = np.array(range(len(y)))
+
+
+    x = np.array(x).astype(float)
+    y = np.array(y).astype(float)
+
+    def linearfit(x2fit, y2fit):
+
+        cur_range = np.array([x2fit.min(), x2fit.max()])
+        tot_range = cur_range[1] - cur_range[0]
+        exp_range = cur_range + ftir * tot_range * np.array([- 1, 1])
+
+        ex = np.linspace(exp_range[0], exp_range[1], 100)
+
+        slope, intercept, r_value, p_value, std_err = stats.linregress(x2fit, y2fit)
+        yy = slope * ex + intercept
+        return slope, intercept, ex, yy
+
+    if len(x) < 3:
+        return np.nan, np.nan
+
+    graph_calc = {'linear': linearfit}
+    if graph_type in graph_calc:
+
+        slope, intercept, ex, yy = graph_calc[graph_type](x, y)
+    else:
+        raise Exception("we can't do that graph type yet")
+
+    if marker_style:
+        if type(marker_style) == str:
+            plot(x, y, marker_style)
+        else:
+            plot(x, y, **marker_style)
+    if line_style:
+        if type(line_style) == str:
+            plot(ex, yy, '--g', linewidth=3)
+        else:
+            plot(ex, yy, '--g', **line_style)
+
+    return slope, intercept
+
+def test_logfit():
+    logfit(np.arange(10), 2 * np.random.randn(10) + np.arange(10),
+           marker_style={'color': 'r', 'marker': None},
+           line_style={'color': 'g', 'lw': 3})
+
+
+# stream_graph
+def streamgraph(df, smooth=None, normalize=None,
+                wiggle=None, label_dict=None, color=None,
+                order=True, linewidth=0.5):
+    # reorder a list from inside out, for use with streamgraph
+    def reorder_idx(idxs):
+        idxs = list(np.flip(idxs))
+        for ii in np.flip(np.arange(1, len(idxs), 2)):
+            idxs.append(idxs.pop(ii))
+        return np.array(idxs)
+
+    #   normalize for use with streamgraph so everything sums to 1
+    def normalize_y(y):
+        return 100 * y / y.sum(axis=0)
+
+    #     interpret wiggle and normalize params for cool defaults
+    #     if not wiggle:
+    #         if normalize:
+
+    #     curate the data:
+    #     add stupid col for who knows why - maybe can leave it out ..
+    df['dumb'] = 'stoopid'
+    cur_cols = df.columns
+    df = df[[cur_cols[2], cur_cols[0], cur_cols[1]]]
+    cur_datas_grouped = df.groupby([cur_cols[0], cur_cols[1]], sort=False).count().unstack()
+    cur_clean = cur_datas_grouped.fillna(0).transpose().sort_index(axis=1)
+
+    cur_labels = np.array([w[1] for w in list(cur_clean.index)])
+
+    if len(cur_labels) > 500:
+        raise Exception("Whoa dude - you are stacking too many things (" + str(len(cur_labels)) + ")")
+
+    # prepare that sweet sweet data
+    x = list(cur_clean.columns)
+    y = np.array(cur_clean)
+
+    #     smooth it so it isn't so choppy
+    if smooth:
+        y = np.array([nan_smooth(w, smooth) for w in y])
+
+    #     normalize it so it takes up the whole graph
+    #     remember to set the y'lims below - or unset the autoscale y
+    if normalize:
+        y = normalize_y(y)
+
+    # prepare the colors for plotting
+    if not color:
+        #   Maybe do something depending on "N here?"
+        color = 'muted'
+    if type(color) == str:
+        pallet_list = sns.color_palette(color, n_colors=len(y))
+        pallet_dict = {k: pallet_list[ii] for ii, k in enumerate(cur_labels)}
+    elif type(color) == sns.palettes._ColorPalette:
+        pallet_list = color
+        pallet_dict = {k: pallet_list[ii] for ii, k in enumerate(cur_labels)}
+    elif type(color) == dict:
+        pallet_dict = color
+
+    if wiggle == None:
+        wiggle = True
+
+    #   prepare the idxs (if you'll wiggle)
+    if order:
+        if type(order) == list:
+            idxs = [cur_labels.tolist().index(w) for w in order]
+        #             print(idxs)
+        else:
+            idxs = np.argsort(np.amax(y, axis=1))
+            if wiggle in [True, 'wiggle', 'streamgraph', 'stream', 'themeriver', 'river']:
+                idxs = reorder_idx(idxs)[::-1]
+    else:
+        idxs = np.arange(len(y))
+
+    labels_reord = cur_labels[idxs]
+    cur_palette = [pallet_dict[w] for w in labels_reord]
+
+    if wiggle == True:
+        cur_base = 'wiggle'
+    elif wiggle == False:
+        cur_base = 'zero'
+    elif wiggle in ['streamgraph', 'stream', 'themeriver', 'river']:
+        cur_base = 'weighted_wiggle'
+    else:
+        cur_base = wiggle
+
+    #   do the plot!
+    ax = plt.stackplot(x, y[idxs], labels=labels_reord,
+                       baseline=cur_base, colors=cur_palette, lw=linewidth, edgecolor='k')
+
+    plt.autoscale(enable=True, axis='x', tight=True)
+    gca().set_yticklabels(np.abs(gca().get_yticks()).astype(int))
+
+    lgnd = legend(labels_reord)
+    gca().legend(loc='center left', bbox_to_anchor=(1, 0.5), framealpha=0.0)
+
+    # nicefy()
+    plt.style.use('classic')
+
+    return gca()
+
+## example data for use in streamgraph:
+# index   date column, grouped column
+# 11623   2011-06-01  United States
+# 13117   2011-07-01         Europe
+# 13118   2011-06-01  United States
+# 13120   2011-07-01        Oceania
+# 13121   2011-08-01         Canada
+# 13122   2011-12-01  United States
+# 13123   2011-08-01  United States
+# 13125   2011-10-01         Canada
+# 65169   2011-08-01         Canada
+# 13126   2011-09-01  United States
+# 65170   2011-09-01         Canada
+
+
+
+
+
+
+
+
+# Does this work? can it be used inside nicefy?
+# def set_fontsize(f_size=15, ax='none'):
+#     if ax == 'none':
+#         ax = plt.gca()
+#
+#     [w.set_fontsize(f_size) for w in ([ax.title, ax.xaxis.label, ax.yaxis.label] +
+#                                                     ax.get_xticklabels() +
+#                                                     ax.get_yticklabels())]
+#     return ax
+
+
+
+def nicefy(f_size=15, clean_legend=True, cur_fig=None, background = 'white'):
+    '''
+    make the figure nicer in general, like ready to be printed etc.
+    '''
+
+    if cur_fig:
+        fig = cur_fig
+    else:
+        fig = plt.gcf()
+
+    # fig.set_tight_layout(True)
+    if background == 'black':
+        fig.set_facecolor('black')
+        plt.style.use('dark_background')
+    elif background  == 'white':
+        fig.set_facecolor('white')
+        plt.style.use('classic')
+    else:
+        plt.style.use(background)
+
+
+    # ['seaborn-darkgrid', 'Solarize_Light2', 'seaborn-notebook', 'classic', 'seaborn-ticks', 'grayscale', 'bmh',
+    #  'seaborn-talk', 'dark_background', 'ggplot', 'fivethirtyeight', '_classic_test', 'seaborn-colorblind',
+    #  'seaborn-deep', 'seaborn-whitegrid', 'seaborn-bright', 'seaborn-poster', 'seaborn-muted', 'seaborn-paper',
+    #  'seaborn-white', 'fast', 'seaborn-pastel', 'seaborn-dark', 'tableau-colorblind10', 'seaborn',
+    #  'seaborn-dark-palette']
+
+    axes = fig.get_axes()
+    # remove the grids
+    # [ax.grid(False) for ax in axes]
+
+    for ax in axes:
+        ax.xaxis.set_label_text(make_title(ax.xaxis.label.get_text()))
+        ax.yaxis.set_label_text(make_title(ax.yaxis.label.get_text()))
+        ax.set_title(make_title(ax.title.get_text()))
+
+        [w.set_fontsize(f_size) for w in ([ax.title, ax.xaxis.label, ax.yaxis.label] +
+                                          ax.get_xticklabels() +
+                                          ax.get_yticklabels())]
+
+    [plt.setp(ax.get_legend().get_texts(), fontsize=f_size) for ax in axes if ax.get_legend()]
+
+    ax = gca()
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+
+    plt.rcParams["figure.figsize"] = [12, 9]
+    plt.rcParams['image.cmap'] = 'viridis'
+
+    if clean_legend:
+        plt.legend(framealpha=0.0)
+
+
+
+
+
+def plot_endpoints(endpoints):
+
+        x_starts = [w[0] for w in endpoints]
+        x_ends = [w[1] for w in endpoints]
+
+        # Set figure size
+        plt.figure(figsize=(16, 8))
+
+        # Make a subplot for each day highlighting registered, filtered and
+        # Get timestamp for star and end of each day
+        x_start = min(x_starts)
+        x_end = max(x_ends)
+
+        # Make a subplot for each day
+        # plt.ylabel(day.strftime('%Y-%m-%d'), rotation=0, labelpad= 30)
+
+        # Get event for each date
+        for idx, event in enumerate(endpoints):
+            plt.fill_between(
+                [event[0], event[1]],
+                [1, 1],
+                color='#0093e7', label='Event')
+
+        plt.ylim((0, 1))
+        plt.xlim((x_start, x_end))
+
+        # Eliminate spaces between subplots
+        # plt.subplots_adjust(hspace=0)
+
+        # Create color patches for the legend
+        # Is this right???
+        red_patch = plt.patches.Patch(color='#0093e7', label='Detected')
+
+        # Plot legend on the bottom subplot
+        plt.legend(loc='lower left', borderaxespad=0., prop={'size': 12},
+                   ncol=3, fancybox=True, shadow=True, handles=[red_patch])
+
+
+
+
+
+def format_axis_date(rot=77):
+    plt.xticks(rotation=rot)
+    # ax.format_xdata = mdates.DateFormatter('%Y-%m-%d')
+    # plt.xticks(x,tick_labels)
+    # plt.xticks(rotation=70)
