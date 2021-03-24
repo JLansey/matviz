4117d06d138af1eed94e8d1cf31f54fb0b078c58
Jonathan Lansey <lansey+github@gmail.com>
2021-03-24 12:14:04 -0400

    releast new version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ MAINTAINER_EMAIL = 'jonathan@lansey.net'
 URL = 'https://github.com/JLansey/matviz'
 LICENSE = 'BSD (3-clause)'
 DOWNLOAD_URL = 'https://github.com/JLansey/matviz'
-VERSION = 'v0.0.4'
+VERSION = 'v0.0.6'
 
 INSTALL_REQUIRES = [
     'numpy>=1.9.3',
