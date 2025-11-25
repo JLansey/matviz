3fcc47159fa2a2a809f4d559e1c49acad4c43d98
Jonathan Lansey <jlansey@cmtelematics.com>
2025-11-25 00:42:07 -0500

    brought back the auto-versioning

--- matviz/__init__.py
+++ matviz/__init__.py
@@ -1,1 +1,5 @@
-from ._version import __version__
+try:
+    from ._version import __version__
+except ImportError:
+    # For development installs where setuptools_scm hasn't generated _version.py yet
+    __version__ = "0.0.0+dev"
